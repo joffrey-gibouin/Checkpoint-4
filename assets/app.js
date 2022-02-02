5d949419d736fbf104542cf2a6410bdb5982146f
joffrey-gibouin <joffreygibouin@gmail.com>
2022-02-02 14:44:15 +0100

    add fontawesome

--- assets/app.js
+++ assets/app.js
@@ -10,4 +10,7 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all');
 require('bootstrap');
